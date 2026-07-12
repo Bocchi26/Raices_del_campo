85bb406928c75fa16f6c15f87bfff399494483b8
Diegoo-10 <patinodiego1005@gmail.com>
2026-07-12 15:48:52 -0500

    Flujo completo de autenticación con JWT. Agregados registro, login y middleware JWT

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,13 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/auth.controller');
-
+const authMiddleware = require('../middlewares/auth.middleware');
 
 
 router.post('/register', authController.register);
 
 router.post('/login', authController.login);
 
+router.get('/profile', authMiddleware, (req, res) => {
+
+    res.status(200).json({
+        mensaje: 'Acceso autorizado',
+        usuario: req.user
+    });
+
+});
 
 
 module.exports = router;
